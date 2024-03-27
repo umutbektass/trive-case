09ac5f1c961a4c26ddc8890f993359fdb21abfb3
umutbektass <umutbektass.78@hotmail.com>
2024-03-28 02:03:19 +0300

    added user list data not found fallback

--- src/app/[locale]/components/account-list/user-list/index.tsx
+++ src/app/[locale]/components/account-list/user-list/index.tsx
@@ -5,43 +5,68 @@ import Link from "next/link";
 function UserList({users,loading , removeUserFromUserList} : {users:User[] , loading:boolean , removeUserFromUserList:Function}) {
     return (
         <div className="col-lg-8" id={styles.userList_Container}>
-            {
-              loading ?
-                <div className='w-100 h-100 d-flex flex-column justify-content-center align-items-center'>
-                  <div className="spinner-border text-primary " role="status">
+           {
+  loading ? (
+    <div className='w-100 h-100 d-flex flex-column justify-content-center align-items-center'>
+      <div className="spinner-border text-primary " role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
+      <p className='mt-1 text-black'>Loading...</p>
+    </div>
+  ) : (
+    <>
+      {users.length === 0 ? (
+       <table className="table p-4 mt-3">
+        <thead>
+        <tr>
+              <th scope="col">#</th>
+              <th scope="col">Full name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone</th>
+            </tr>
+        </thead>
+        <tbody>
+          <td >Data bulunamadı...</td>
+        </tbody>
+       </table>
+      ) : (
+        <table className="table p-4 mt-3">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Full name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((item, key) => {
+              return (
+                <tr key={key}>
+                  <th scope="row">{item.id}</th>
+                  <td>{item.name}</td>
+                  <td>{item.email}</td>
+                  <td>{item.phone}</td>
+                  <td>
+                    <Link
+                      onClick={() => removeUserFromUserList(item.id)}
+                      className='text-decoration-none'
+                      href={''}
+                      scroll={false}
+                    >
+                      <p className='text-danger'>Sil</p>
+                    </Link>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      )}
+    </>
+  )
+}
 
-                    <span className="sr-only">Loading...</span>
-
-                  </div>
-                  <p className='mt-1'>Yükleniyor,lütfen bekleyiniz...</p>
-                </div>
-
-                : <table className="table p-4 mt-3">
-                  <thead>
-                    <tr>
-                      <th scope="col">#</th>
-                      <th scope="col">Full name</th>
-                      <th scope="col">Email</th>
-                      <th scope="col">Phone</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {
-                      users.map((item, key) => {
-                        return (
-                          <tr key={key} >
-                            <th scope="row">{item.id}</th>
-                            <td>{item.name}</td>
-                            <td>{item.email}</td>
-                            <td>{item.phone}</td>
-                            <td><Link onClick={() => removeUserFromUserList(item.id)} className='text-decoration-none' href={''} scroll={false}><p className='text-danger'>Sil</p></Link></td>
-                          </tr>
-                        )
-                      })
-                    }
-                  </tbody>
-                </table>
-            }
           </div>
     );
 }
